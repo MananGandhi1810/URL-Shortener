ad62709dc0dd91889306472aee7a0bef70c78f22
Manan Gandhi <ardumanan@gmail.com>
2023-11-22 19:35:32 +0530

    Complete

---
+++ README.md
@@ -0,0 +1,12 @@
+# URL Shortener
+## Description
+A simple URL shortener built with Flask and JSON file as a storage system.
+
+## Installation
+1. Clone the repository
+2. Install the requirements
+3. Customize the routes.json file
+4. Run the main.py file
+
+## Release
+The project is hosted on Render and can be accessed at [https://go.manangandhi.tech/](https://go.manangandhi.tech/)
---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, redirect
+import json
+
+routes = json.load(open("routes.json", "r"))
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return redirect("https://manangandhi.tech/")
+
+
+@app.route("/<name>")
+def resume(name):
+    if name in routes:
+        return redirect(routes[name])
+    return redirect("https://manangandhi.tech/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
